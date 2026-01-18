35f44a2bc4c73926ab06f0865b961d908ed8d9a9
Mimo1103 <michim07@web.de>
2026-01-18 16:28:53 +0100

    t

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hi!")
+import sys
+from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QPixmap, QPainter, QPen
+
+
+class Menu(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.drawing = False
+        self.lastPoint = QPoint()
+        self.image = QPixmap("image.png")
+        self.setGeometry(100, 100, 500, 300)
+        self.resize(self.image.width(), self.image.height())
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainMenu = Menu()
+    sys.exit(app.exec_())
